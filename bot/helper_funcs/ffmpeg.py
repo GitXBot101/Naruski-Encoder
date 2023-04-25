c4db7ae0bdd429b45672d8a429b3e8c21316a6fb
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-04-25 08:41:38 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -38,13 +38,14 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
     ##"-metadata", "title=@SenpaiAF", "-vf", "drawtext=fontfile=njnaruto.ttf:fontsize=20:fontcolor=black:x=15:y=15:text=" "Dark Encodes",
      ## -vf eq=gamma=1.4:saturation=1.4
      ## lol 😂
-    crf.append("26.2 -map 0")
+    crf.append("30")
+    codec.append("libx265")
     resolution.append("1280x720")
-    bit.append("yuv420p")
-    preset.append("veryfast")
-    watermark.append('-vf "drawtext=fontfile=font.ttf:fontsize=25:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:box=1:boxcolor=black@0.5:boxborderw=6:text=FIERCENETWORK"')
-    file_genertor_command = f'ffmpeg -hide_banner -loglevel quiet -progress "{progress}" -i "{video_file}" -c:v libx265  -crf {crf[0]} -c:s copy -preset fast -pix_fmt yuv420p10le -vf scale=1280:trunc(ow/a/2)*2 -x265-params no-info=1 -metadata title="Moviez Café™ | 720p.10bit.HEVC.x265"  -metadata:s:v title="W∆L13R - 720p/ 10bit/ HEVC"  -metadata:s:a title="Opus ~ W∆L13R" -metadata:s:s title="English ~ W∆L13R" -c:a libopus -b:a 64k "{out_put_file_name}" -y'
- #For Ffmpeg Use
+    preset.append("ultrafast")
+    audio_b.append("96k")
+    watermark.append('-vf "drawtext=fontfile=font.ttf:fontsize=25:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:box=1:boxcolor=black@0.5:boxborderw=6:text=〄"')
+    file_genertor_command = f'ffmpeg -hide_banner -loglevel quiet -progress "{progress}" -i "{video_file}" {watermark[0]}  -c:v {codec[0]}  -map 0 -crf {crf[0]} -c:s copy -pix_fmt yuv420p -s {resolution[0]} -b:v 150k -c:a libopus -b:a {audio_b[0]} -preset {preset[0]}  "{out_put_file_name}" -y'
+ #Done !
     COMPRESSION_START_TIME = time.time()
     process = await asyncio.create_subprocess_shell(
           file_genertor_command,
